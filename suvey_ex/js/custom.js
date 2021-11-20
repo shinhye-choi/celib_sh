f6e4daef89d8523ecc759a0e05ea4643b1eead8e
shinhye <cine_c@celib.kr>
2021-11-21 08:45:21 +0900

    dd

--- suvey_ex/js/custom.js
+++ suvey_ex/js/custom.js
@@ -1,43 +1,53 @@
 
-function scrollUp() {
-    const vheight = $('.test').height();
+function scrollUp(){
+    const pageheight = $('.test').height();
     $('html, body').animate({
-      scrollTop: ((Math.ceil($(window).scrollTop() / vheight)-1) * vheight)
-    }, 500);
+        scrollTop: ((Math.floor($(window).scrollTop() / pageheight)-1) * pageheight)
+    }, 900);
 };
 
-function scrollDown() {
-    const vheight = $('.test').height();
+function scrollDown(){
+    const pageheight = $('.test').height();
+    console.log('aaaaa');
     $('html, body').animate({
-        scrollTop: ((Math.floor($(window).scrollTop() / vheight)+1) * vheight)
-    }, 500);
-}
+        scrollTop: ((Math.floor($(window).scrollTop() / pageheight)+1) * pageheight)
+    }, 900);
+    console.log('ccccc');
+};
 
 $(function(){
-    $('.next_btn').click(function(e){
-        let divs = $(this).parent().prev().children();
-        let inputs = divs.find('input:checked');
+    $('.next').click(function(event){
+        const divs = $(this).parent().prev().children();
+        console.log('11111');
+        const inputs = divs.find('input:checked');
+        console.log('22222');
         if(inputs.length < 1) {
+            console.log('33333');
+            alert('문항이 선택되지 않았습니다.');
             return false;
+            
         }
-        e.preventDefault();
+        console.log('44444');
+        event.preventDefault();
+        console.log('55555');
         scrollDown();
     });
 
-    $('.prev_btn').click(function(e){
-        e.preventDefault();
+    $('.prev').click(function(event){
+        event.preventDefault();
         scrollUp();
     });
 
     $("#form").submit(function() {
-        let radios = $('input[type=radio]:checked');
-        if(radios.length < 10) {
+        const radios = $('input[type=radio]:checked');
+        if(radios.length < 3) {
+            alert("문항이 선택되지 않았습니다.");
             return false;
         }
+        alert('설문제출완료 ! ')
         return true;
     });
 
     $("html, body").animate({
-        scrollTop: 0
-    }, 500);
+        scrollTop: 0});
 });
